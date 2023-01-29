fb8eec0f17a1742835eadfbdd594c3532a16b9cf
TSShrol <shrolts@gmail.com>
2023-01-29 20:52:35 +0200

    Change CategoryService

--- SS_EDUP.Core/Services/CategoryService.cs
+++ SS_EDUP.Core/Services/CategoryService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SS_EDUP.Core.Entities;
+using SS_EDUP.Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,41 @@ using System.Threading.Tasks;
 
 namespace SS_EDUP.Core.Services
 {
-    internal class CategoryService
+    public class CategoryService : ICategoryService
     {
+        private readonly IRepository<Category> categoryRepo;
+        public CategoryService(IRepository<Category> categoryRepo)
+        {
+           this.categoryRepo = categoryRepo;
+        }
+        public void Create(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category? Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

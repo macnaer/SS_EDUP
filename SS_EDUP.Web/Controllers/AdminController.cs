8a0bba175b0bab26c6574beb3c25faa44664aaa7
macnaer <trofimchuk.an@gmail.com>
2023-01-30 20:32:57 +0200

    Create dashboard

---
+++ SS_EDUP.Web/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SS_EDUP.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult Profile() { 
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}

392672fc6f061c51f075a904e373e3c8e1defc85
TSShrol <shrolts@gmail.com>
2023-03-04 23:11:50 +0200

    Modified Core

---
+++ SS_EDUP.Core/DTO's/LearningDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS_EDUP.Core.DTO_s
+{
+    public class LearningDto
+    {
+        public int Id { get; set; }
+        public int CourseID { get; set; }
+        //studentid
+        public string AppUserId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int Progress { get; set; }
+    }
+}

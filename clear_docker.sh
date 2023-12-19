3f5ef48583c1a344af653b16a1b592e730023a6b
macnaer <trofimchuk.an@gmail.com>
2023-12-19 17:39:58 +0200

    Update clear_docker.sh

--- clear_docker.sh
+++ clear_docker.sh
@@ -4,7 +4,7 @@ if [[$! == 0]];then
         docker kill $(docker ps -q)
 fi
 docker rm $(docker ps -aq) 2> /dev/null
-if [[$! == 0]];then
+if [[ $! == 0 ]];then
         docker rm $(docker ps -aq)
 fi
 yes | docker image prune -a
